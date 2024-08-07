bc2a60471b89b168e165ccdba62522e067699d5b
Pavan-KumarCH <pavun9848@gamil.com>
2024-08-08 00:45:31 +0530

    New Changes Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Application",
+    version = "1.0",
+    author = "Pavun-KumarCH",
+    author_email = "pavun9848@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+    )
