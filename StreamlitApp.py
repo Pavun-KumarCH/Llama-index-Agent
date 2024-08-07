d099ef7e67b8ad7616ff051af570933d16af2061
Pavan-KumarCH <pavun9848@gamil.com>
2024-08-08 00:57:21 +0530

    Added Streamlit

---
+++ StreamlitApp.py
@@ -0,0 +1,26 @@
+import StreamlitApp as st
+from Components.data_ingestion import load_data
+from Components.embedding import download_gemini_embedding
+from Components.model_api import load_model
+
+def main():
+    st.set_page_config(page_title = "University Course Documents")
+    st.image("assets/LlamaIndex.jpg", use_column_width=True)
+    st.header("Chat with Intelligent Agent using Gemini💁")
+
+    pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                document = load_data(doc)
+                model = load_model()
+                query_engine = download_gemini_embedding(model, document)
+                st.success("Done")
+                response = query_engine.query(user_question)
+                st.write(response.response)
+
+if __name__ == "__main__":
+     main()
+
